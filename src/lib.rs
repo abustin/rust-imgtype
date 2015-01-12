19dbc75dbe37b770a49568d472c6059354285a65
Alex Bustin <bustin@gmail.com>
2015-01-12 12:12:28 -0800

    updated rust nightly version

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,27 @@
-#[deriving(Show)]
+#[derive(Show, Copy)]
 pub enum ImageType { JPEG, PNG, WEBP, GIF, TIFF }
 
-static JPEG_SPEC:[Option<u8>, ..11] = [Some(0xFF), Some(0xD8), Some(0xFF), Some(0xE0), 
-                                       None,       None,       Some(0x4A), Some(0x46), 
-                                       Some(0x49), Some(0x46), Some(0x00)];
+static JPEG_SPEC:[Option<u8>;11] = [Some(0xFF), Some(0xD8), Some(0xFF), Some(0xE0), 
+                                    None,       None,       Some(0x4A), Some(0x46), 
+                                    Some(0x49), Some(0x46), Some(0x00)];
 
-static JPEG_EXIF:[Option<u8>, ..11] = [Some(0xFF), Some(0xD8), Some(0xFF), Some(0xE1),
-                                       None,       None,       Some(0x45), Some(0x78), 
-                                       Some(0x69), Some(0x66), Some(0x00)];
+static JPEG_EXIF:[Option<u8>;11] = [Some(0xFF), Some(0xD8), Some(0xFF), Some(0xE1),
+                                    None,       None,       Some(0x45), Some(0x78), 
+                                    Some(0x69), Some(0x66), Some(0x00)];
 
-static PNG_SPEC:[Option<u8> ,..8]   = [Some(0x89), Some(0x50), Some(0x4E), Some(0x47), 
-                                       Some(0x0D), Some(0x0A), Some(0x1A), Some(0x0A)];
+static PNG_SPEC:[Option<u8>;8]   = [Some(0x89), Some(0x50), Some(0x4E), Some(0x47), 
+                                    Some(0x0D), Some(0x0A), Some(0x1A), Some(0x0A)];
 
-static WEBP_SPEC:[Option<u8>, ..12] = [Some(0x52), Some(0x49), Some(0x46), Some(0x46), 
-                                       None,       None,       None,       None, 
-                                       Some(0x57), Some(0x45), Some(0x42), Some(0x50)];
+static WEBP_SPEC:[Option<u8>;12] = [Some(0x52), Some(0x49), Some(0x46), Some(0x46), 
+                                    None,       None,       None,       None, 
+                                    Some(0x57), Some(0x45), Some(0x42), Some(0x50)];
 
-static GIF_SPEC:[Option<u8>, ..6]   = [Some(0x47), Some(0x49), Some(0x46), Some(0x38), 
-                                       None,       Some(0x61)];
+static GIF_SPEC:[Option<u8>;6]   = [Some(0x47), Some(0x49), Some(0x46), Some(0x38), 
+                                    None,       Some(0x61)];
 
-static TIFF_SPEC:[Option<u8>, ..3]   = [Some(0x49), Some(0x20), Some(0x49)];
+static TIFF_SPEC:[Option<u8>;3]   = [Some(0x49), Some(0x20), Some(0x49)];
 
-static FILE_TESTS:[(ImageType, &'static [Option<u8>]), ..6] = [
+static FILE_TESTS:[(ImageType, &'static [Option<u8>]);6] = [
     (ImageType::JPEG, &JPEG_SPEC),
     (ImageType::JPEG, &JPEG_EXIF),
     (ImageType::PNG , &PNG_SPEC ),
@@ -32,7 +32,7 @@ static FILE_TESTS:[(ImageType, &'static [Option<u8>]), ..6] = [
 
 pub fn from_buffer(file:&[u8]) -> Option<ImageType> {
     match FILE_TESTS.iter().filter(|aspec| {
-        file.iter().zip(aspec.val1().iter()).all(|file_spec| {
+        file.iter().zip((aspec.1).iter()).all(|file_spec| {
             let (file,spec) = file_spec;
             match spec {
                 &Some(bit) => (bit == *file),
@@ -40,7 +40,7 @@ pub fn from_buffer(file:&[u8]) -> Option<ImageType> {
             }
         })
     }).next() {
-        Some(found) => Some(found.val0()),
+        Some(found) => Some(found.0),
         None => None
     }    
 }
@@ -52,7 +52,7 @@ fn it_works() {
     let img_type:Option<ImageType> = from_buffer(&file);
 
     match img_type {
-        Some(img_type) => println!("Image Type is {}!", img_type),
+        Some(img_type) => println!("Image Type is {:?}!", img_type),
         None           => println!("Unknown Image Type.")
     };
 }
